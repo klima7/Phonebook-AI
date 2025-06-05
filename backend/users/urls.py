8190d2beb97f88171b1dde55d50ef22550640b1e
klima7 <github@mail.ukasz.com>
2025-06-06 01:02:52 +0200

    backend: add /me url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken import views
+from .views import CurrentUserView
 
 urlpatterns = [
-    path('token/', views.obtain_auth_token)
+    path('token/', views.obtain_auth_token, name='create-token'),
+    path('me/', CurrentUserView.as_view(), name='current-user')
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
