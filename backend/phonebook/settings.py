bd40df260979aeb8917c92f0bf7734b8e741764f
klima7 <github@mail.ukasz.com>
2025-06-06 01:06:05 +0200

    backend: add default permission classes for authenticated users

--- backend/phonebook/settings.py
+++ backend/phonebook/settings.py
@@ -133,7 +133,10 @@ FORCE_SCRIPT_NAME = '/api'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 SWAGGER_SETTINGS = {
