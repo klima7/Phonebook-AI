52a82faa819b776be494ebf99e25ca62bd1151f1
klima7 <github@mail.ukasz.com>
2025-06-06 23:32:21 +0200

    Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "~": resolve(__dirname, "./app"),
