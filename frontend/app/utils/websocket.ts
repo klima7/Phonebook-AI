fc9715b9203b7dc6ff2aba77386a530601ece38a
klima7 <github@mail.ukasz.com>
2025-06-08 17:23:48 +0200

    cleanup websockets

--- frontend/app/utils/websocket.ts
+++ frontend/app/utils/websocket.ts
@@ -6,39 +6,18 @@ import { useAuth } from '../hooks/useAuth';
 export function useAuthedWebSocket(
   url: string | (() => Promise<string>),
   options: Options = {},
-  shouldConnect: boolean = true
 ) {
   const { token, isAuthenticated } = useAuth();
-  
-  // Create the authentication subprotocol
-  const authProtocol = token ? `token.${token}` : undefined;
-  
-  // Combine any user-provided protocols with our auth protocol
-  let protocols: string | string[] | undefined = undefined;
-  
-  if (authProtocol) {
-    if (options.protocols) {
-      if (Array.isArray(options.protocols)) {
-        protocols = [...options.protocols, authProtocol];
-      } else {
-        protocols = [options.protocols, authProtocol];
-      }
-    } else {
-      protocols = [authProtocol];
-    }
-  } else {
-    protocols = options.protocols;
-  }
+  const protocols = ["authorization", `token.${token}`];
 
-  protocols = ["authorization", ...(protocols || [])];
-  
-  // Only connect if authenticated (unless explicitly overridden)
-  const shouldConnectWithAuth = shouldConnect && isAuthenticated;
+  if (!isAuthenticated) {
+    throw new Error("Unable to connect to websocket: Not authenticated");
+  }
   
   return useWebSocket(url, {
     ...options,
     protocols,
-  }, shouldConnectWithAuth);
+  });
 }
 
 export { ReadyState } from 'react-use-websocket';
