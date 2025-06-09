ff36a3035128f7df017ed7878157526caf42de87
klima7 <github@mail.ukasz.com>
2025-06-09 18:28:13 +0200

    Remove duplicated middleware

--- backend/phonebook/settings.py
+++ backend/phonebook/settings.py
@@ -58,8 +58,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
