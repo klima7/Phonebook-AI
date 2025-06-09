b53f268ca7f834e1a3eeeab64865ee0db26040ff
klima7 <github@mail.ukasz.com>
2025-06-09 18:17:37 +0200

    Add missed cors headers

--- backend/phonebook/settings.py
+++ backend/phonebook/settings.py
@@ -58,6 +58,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
